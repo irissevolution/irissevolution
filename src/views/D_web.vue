23d2e7011f13edbb50fe083ee61faa554fc600e7
Luis1999s <bejamarly@gmail.com>
2022-06-05 14:29:31 -0500

    onePage

--- src/views/D_web.vue
+++ src/views/D_web.vue
@@ -1,3 +1,125 @@
 <template>
-  <div class="bg-red-300 text-black-600 font-mono text-3xl font-bold">D_web</div>
+  <div class="text-black-600 font-mono text-3xl font-bold p-10">¿Quienes somos?</div>
+  <div class="containerD_web">
+    <div>
+      <p>Somos las empresa #1 en el mercado</p>
+      <p class="p-6">Contamos ocn desarrolladores de software del más alto rango</p>
+      <button class="btn-primary">
+        ¡Contactar!
+      </button>
+    </div>
+    <img class="width_1" alt="Iriss Logo" src="../assets/1.png">
+  </div>
+  <div class="bg-red-300 text-black-600 font-mono text-3xl font-bold">Desarrollo WEB</div>
+  <div class="containerD_web">
+      <div class=" px-0 py-2 row-span-2 col-span-2 text-black w-96 my-10 border-8 border-indigo-600">
+          <div>
+              <img class="h-72 m-auto mb-5" src="@/assets/lavame.jpeg" >
+          </div>
+          <a class=" px-5 font-bold" href="https://irissevolution.github.io/lavamemucho/index2.html" target="_blank">
+              Lávame mucho lavanderías (click aquí)
+          </a>
+          <div class=" px-5 py-5 pr-7">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et 
+              soluta praesentium enim error ipsa obcaecati qui quaerat,
+              omnis est animi amet atque dicta nobis quia facilis! Numquam 
+              harum facere rerum?
+          </div>
+          <div class="flex  justify-around text-red-700">
+              <div class="bg-blue-200 rounded-full w-28 text-center">
+                  #Photography
+              </div>
+              <div class="bg-blue-200 rounded-full w-20 text-center">
+                  #travel
+              </div>
+              <div class="bg-blue-200 rounded-full w-20 text-center">
+                  #winter
+              </div>
+          </div> 
+      </div>
+  </div>
+  <div class="bg-red-300 text-black-600 font-mono text-3xl font-bold">Chatbot</div>
+  <div class="containerD_web">
+      <div class=" px-0 row-span-2 col-span-2 text-black w-96 m-auto">
+          <div>
+              <img class="h-72 m-auto mb-5" src="@/assets/lavame.jpeg" >
+          </div>
+          <a class=" px-5 font-bold" href="https://irissevolution.github.io/lavamemucho/index2.html" target="_blank">
+              Lávame mucho lavanderías (click aquí)
+          </a>
+          <div class=" px-5 py-5 pr-7">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et 
+              soluta praesentium enim error ipsa obcaecati qui quaerat,
+              omnis est animi amet atque dicta nobis quia facilis! Numquam 
+              harum facere rerum?
+          </div>
+          <div class="flex  justify-around text-red-700">
+              <div class="bg-blue-200 rounded-full w-28 text-center">
+                  #Photography
+              </div>
+              <div class="bg-blue-200 rounded-full w-20 text-center">
+                  #travel
+              </div>
+              <div class="bg-blue-200 rounded-full w-20 text-center">
+                  #winter
+              </div>
+          </div> 
+      </div>
+  </div>
+  <div class="bg-red-300 text-black-600 font-mono text-3xl font-bold">Robótica</div>
+  <div class="containerD_web">
+      <div class=" px-0 row-span-2 col-span-2 text-black w-96 m-auto">
+          <div>
+              <img class="h-72 m-auto mb-5" src="@/assets/lavame.jpeg" >
+          </div>
+          <a class=" px-5 font-bold" href="https://irissevolution.github.io/lavamemucho/index2.html" target="_blank">
+              Lávame mucho lavanderías (click aquí)
+          </a>
+          <div class=" px-5 py-5 pr-7">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et 
+              soluta praesentium enim error ipsa obcaecati qui quaerat,
+              omnis est animi amet atque dicta nobis quia facilis! Numquam 
+              harum facere rerum?
+          </div>
+          <div class="flex  justify-around text-red-700">
+              <div class="bg-blue-200 rounded-full w-28 text-center">
+                  #Photography
+              </div>
+              <div class="bg-blue-200 rounded-full w-20 text-center">
+                  #travel
+              </div>
+              <div class="bg-blue-200 rounded-full w-20 text-center">
+                  #winter
+              </div>
+          </div> 
+      </div>
+  </div>
+  <div class="bg-red-300 text-black-600 font-mono text-3xl font-bold">Contacto</div>
+  <div class="containerD_web">
+      <div class=" px-0 row-span-2 col-span-2 text-black w-96 m-auto">
+          <div>
+              <img class="h-72 m-auto mb-5" src="@/assets/lavame.jpeg" >
+          </div>
+          <a class=" px-5 font-bold" href="https://irissevolution.github.io/lavamemucho/index2.html" target="_blank">
+              Lávame mucho lavanderías (click aquí)
+          </a>
+          <div class=" px-5 py-5 pr-7">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et 
+              soluta praesentium enim error ipsa obcaecati qui quaerat,
+              omnis est animi amet atque dicta nobis quia facilis! Numquam 
+              harum facere rerum?
+          </div>
+          <div class="flex  justify-around text-red-700">
+              <div class="bg-blue-200 rounded-full w-28 text-center">
+                  #Photography
+              </div>
+              <div class="bg-blue-200 rounded-full w-20 text-center">
+                  #travel
+              </div>
+              <div class="bg-blue-200 rounded-full w-20 text-center">
+                  #winter
+              </div>
+          </div> 
+      </div>
+  </div>
 </template>
